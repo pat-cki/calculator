dc9f7a6c17b1f231825c04cf815af9dda8234806
Pat Cichocki <pat.cichocki.uk@gmail.com>
2019-10-22 17:48:40 +0100

    ui variables declared

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log(123);
+// UI variables
+const display = document.querySelector('.display'),
+  integerButtons = document.querySelectorAll('.integer'), 
+  operationsButtons = document.querySelectorAll('.operation'),
+  decimalButton = document.querySelector('.decimal'),
+  resetButton = document.querySelector('.clear');
